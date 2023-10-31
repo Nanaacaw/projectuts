581555b259da0f76979af62727b10a36aa70773c
NanaCasmana <awnana123@gmail.com>
2023-10-31 23:19:16 +0700

    add bottom nav

---
+++ lib/master.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Master extends StatelessWidget {
+  const Master({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
