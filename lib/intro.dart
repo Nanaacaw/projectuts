0b4ee1baa2aa96fc76a5cda767f713fffc458488
NanaCasmana <awnana123@gmail.com>
2023-11-11 19:32:24 +0700

    add google navbar

--- lib/intro.dart
+++ lib/intro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/home.dart';
 
 class PageIntro extends StatelessWidget {
   const PageIntro({super.key});
@@ -7,50 +8,69 @@ class PageIntro extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: Column(children: [
-        //logo
-        Image(
-          image: AssetImage('lib/images/logo.png'),
-        ),
-
-        SizedBox(height: 20),
-        //title
-        Text(
-          'Shoe.co',
-          style: TextStyle(
-            fontSize: 50,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-
-        SizedBox(height: 18),
-        //sub title
-        Center(
-          child: Text(
-            'Menyediakan Gaya Sepatu yang Tidak Pernah Usang, Langkah penuh gaya bersama Shoe.co',
-            style:
-                TextStyle(fontStyle: FontStyle.italic, color: Colors.grey[600]),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        SizedBox(
-          height: 80,
-        ),
-        //start now button
-        Container(
-          child: Center(
-            child: Text(
-              'Pergi Belanja',
-              style: TextStyle(color: Colors.white),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            //logo
+            Image(
+              image: AssetImage('lib/images/logo.png'),
             ),
-          ),
-          padding: EdgeInsets.all(18),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors.grey[700],
-          ),
-        )
-      ]),
+
+            const SizedBox(height: 30),
+            //title
+            Text(
+              'Shoe.co',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 18),
+            //sub title
+            Center(
+              child: Text(
+                'Menyediakan Gaya Sepatu yang Tidak Pernah Usang, Langkah penuh gaya bersama Shoe.co',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey[600],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            //start now button
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey[700],
+                ),
+                child: const Center(
+                  child: Text(
+                    'Pergi Belanja',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                padding: const EdgeInsets.all(28),
+              ),
+            )
+          ]),
+        ),
+      ),
     );
   }
 }
